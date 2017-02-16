d265df576a2f841c81ee7c3c15c4e14465de8d20
craneliuzhihua <craneliu@163.com>
2017-02-16 13:28:59 +0800

    Update and rename plt_legend_bbox_to_anchor.py to plt-legend-bbox_to_anchor.py

--- plt-legend-bbox_to_anchor.py
+++ plt-legend-bbox_to_anchor.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #---coding:UTF-8---
 #本脚本用来输出网页计费新框架的各个接口最近其他的访问量的图形
+#目前输出的是图例在图片右侧，详细效见图片plt-legend-bbox_to_anchor.png
+#本脚本用到的源数据见文件Last7_total.csv
 #version:1.0.0
 #release:2017/02/15
 #author:lzh phone:15811869179
@@ -28,13 +30,9 @@ with open(filename) as f:
          IntegratedSubscribeReq_req_nums.append(IntegratedSubscribeReq_req_num)
          
 fig=plt.figure(dpi=128, figsize=(20,6),facecolor='yellow',edgecolor='green')
-#apSyncQueryReq,=plt.plot(dates,apSyncQueryReq_req_nums,c='red',alpha=0.5,label='apSyncQueryReq')
-#IntegratedSubscribeReq,=plt.plot(dates,IntegratedSubscribeReq_req_nums,c='blue',alpha=0.5,label='IntegratedSubscribeReq')
-#plt.legend(handles=[apSyncQueryReq, IntegratedSubscribeReq],loc=6)
 plt.plot(dates,apSyncQueryReq_req_nums,c='red',alpha=0.5,label='apSyncQueryReq')
 plt.plot(dates,IntegratedSubscribeReq_req_nums,c='blue',alpha=0.5,label='IntegratedSubscribeReq')
-#plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=5,ncol=2, mode="expand", borderaxespad=0.)
-plt.legend(bbox_to_anchor=(1,.102), loc=10,ncol=1, mode="expand", borderaxespad=0.)
+plt.legend(bbox_to_anchor=(1,.102), loc=10,ncol=1, mode="expand", borderaxespad=0.,fontsize=6)
 
 title="Wabps's Access Of Different Interfaces"
 plt.title(title,fontsize=10)
